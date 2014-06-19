fc54d766f249ca165f1a711ac384fbc7db84300e
= <aaronjg@gmail.com>
2014-06-18 21:11:28 -0700

    Updated to V2 api.  Added support for api key or oauth access token.

--- pushbullet/device.py
+++ pushbullet/device.py
@@ -1,62 +1,76 @@
+from datetime import datetime
 import json
 
 import requests
 
+
 class Device(object):
 
-	PUSH_URL = "https://api.pushbullet.com/api/pushes"
+    PUSH_URL = "https://api.pushbullet.com/v2/pushes"
 
-	def __init__(self, api_key, device_id, device_info = None):
-		self.api_key = api_key
-		self.device_id = device_id
+    def __init__(self, device_id, api_key, access_token, device_info=None):
+        self.api_key = api_key
+        self.access_token = access_token
+        self.auth = access_token if access_token else api_key
+        self.device_id = device_id
+        device_info = device_info or {}
 
-		device_info = device_info or {}
+        self.push_token = device_info.get('push_token')
+        self.app_version = device_info.get('app_version')
+        self.android_sdk_version = device_info.get('android_sdk_version')
+        self.fingerprint = device_info.get('fingerprint')
+        self.active = device_info.get('active', False)
+        self.nickname = device_info.get('nickname')
+        self.manufacturer = device_info.get('manufacturer')
+        self.kind = device_info.get('kind')
+        self.android_version = device_info.get('android_version')
+        self.model = device_info.get('model')
+        self.pushable = device_info.get('pushable')
 
-		self.owner = device_info.get("owner_name")
+        created = device_info.get('created')
+        if created:
+            self.created = datetime.fromtimestamp(created)
+        else:
+            self.created = None
+        modified = device_info.get('modified')
+        if modified:
+            self.modified = datetime.fromtimestamp(modified)
+        else:
+            self.modified = None
 
-		extras = device_info.get("extras", {})
-		self.model = extras.get("model")
-		self.manufacturer = extras.get("manufacturer")
-		self.android_version = extras.get("android_version")
-		self.sdk_version = extras.get("sdk_version")
-		self.app_version = extras.get("app_version")
+        self._fullname = "{} {} {}".format(self.manufacturer,
+                                           self.model, self.android_version)
 
-		self._fullname = "{} {} {}".format(self.manufacturer,
-										   self.model, self.android_version)
+        self.name = self.nickname or self._fullname
 
-		nickname = extras.get("nickname")
-		self.name = nickname or self._fullname
+        self._json_header = {'Content-Type': 'application/json'}
 
-		self._json_header = {'Content-Type': 'application/json'}
+    def push_note(self, title, body):
+        data = {"type": "note", "title": title, "body": body}
+        return self._push(data, headers=self._json_header)
 
-	def push_note(self, title, body):
-		data = {"type": "note", "title": title, "body": body}
-		return self._push(data, headers=self._json_header)
+    def push_address(self, name, address):
+        data = {"type": "address", "name": name, "address": address}
+        return self._push(data, headers=self._json_header)
 
-	def push_address(self, name, address):
-		data = {"type": "address", "name": name, "address": address}
-		return self._push(data, headers=self._json_header)
+    def push_list(self, title, items):
+        data = {"type": "list", "title": title, "items": items}
+        return self._push(data, headers=self._json_header)
 
-	def push_list(self, title, items):
-		data = {"type": "list", "title": title, "items": items}
-		return self._push(data, headers=self._json_header)
+    def push_file(self, file):
+        raise NotImplemented("v2 file pushing isn't implemented")
 
-	def push_file(self, file):
-		data = {"type": "file"}
-		files = {"file": file}
-		return self._push(data, files=files)
+    def push_link(self, title, url, body=None):
+        data = {"type": "link", "title": title, "url": url, "body": body}
+        return self._push(data, headers=self._json_header)
 
-	def push_link(self, title, url):
-		data = {"type": "link", "title": title, "url": url}
-		return self._push(data, headers=self._json_header)
+    def _push(self, data, headers={}, files = {}):
+        data["device_id"] = self.device_id
+        if not files:
+            data = json.dumps(data)
+        headers.update({"User-Agent": "ifttt2pushbullet.herokuapp.com"})
+        return requests.post(self.PUSH_URL, data=data, headers=headers,
+                             files=files, auth=(self.auth, ""))
 
-	def _push(self, data, headers={}, files = {}):
-		data["device_id"] = self.device_id
-		if not files:
-			data = json.dumps(data)
-		headers.update({"User-Agent": "ifttt2pushbullet.herokuapp.com"})
-		return requests.post(self.PUSH_URL, data=data, headers=headers,
-							 files=files, auth=(self.api_key, ""))
-
-	def __repr__(self):
-		return "Device('{}', {})".format(self.api_key, self.device_id)
+    def __repr__(self):
+        return "Device('{}', {})".format(self.api_key, self.device_id)
--- pushbullet/pushbullet.py
+++ pushbullet/pushbullet.py
@@ -12,16 +12,20 @@ else:
 
 class PushBullet(object):
 
-    DEV_LIST_URL = "https://api.pushbullet.com/api/devices"
+    DEVICES_LIST_URL = "https://api.pushbullet.com/v2/devices"
 
-    def __init__(self, api_key):
+    def __init__(self, api_key=None, access_token=None):
         self.api_key = api_key
+        self.access_token = access_token
+        if not self.api_key and not self.access_token:
+            raise Exception("api_key or access_token params required")
+        self.auth = access_token if access_token else api_key
 
         self._devices = []
         self._load_devices()
 
     def _load_devices(self):
-        resp = requests.get(self.DEV_LIST_URL, auth=(self.api_key, ""))
+        resp = requests.get(self.DEVICES_LIST_URL, auth=(self.auth, ""))
         resp_dict = resp.json()
 
         own_devices = resp_dict.get("devices", [])
@@ -29,7 +33,8 @@ class PushBullet(object):
 
         devices = []
         for device_info in chain(own_devices, shared_devices):
-            d = Device(self.api_key, device_info["id"], device_info)
+            d = Device(device_info["iden"], self.api_key, self.access_token,
+                       device_info)
             devices.append(d)
 
         self._devices = devices
